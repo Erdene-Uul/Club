44e7ac9d819a73ea2c3887c23cd95bfc95f6c93f
Munguu <mungu0623@gmail.com>
2022-03-29 02:13:32 -0700

    psdag burmoosn ustgaw

--- src/components/Cover/CoverEffect/index.js
+++ src/components/Cover/CoverEffect/index.js
@@ -9,10 +9,14 @@ import effectThree from "../../../images/effect-3.png";
 const CoverEffect = (props) => {
   return (
     <div className="bg-[#909090]/10 w-full h-screen relative m-auto" id="home">
-      <img src={tsegImg} className="h-32 absolute mt-24 ml-16" />
-      <div className="flex">
-        <Title />
-        <img src={CoverPhoto} className="h-96 mt-12 md:h-512 xl:h-1080" />
+      <div className="mx-28 ">
+        <img src={tsegImg} className="h-32 absolute mt-24 ml-16" />
+        <div className="flex justify-between ">
+          <Title />
+          <div className="flex ">
+            <img src={CoverPhoto} className="h-96 mt-12 md:h-512 xl:h-1080" />
+          </div>
+        </div>
       </div>
       <img src={effectOne} className="h-80 -mt-44 2xl:-mt-80 absolute" />
       <img src={effectTwo} className=" -mt-80 ml-512 absolute" />
--- src/components/Cover/title/index.js
+++ src/components/Cover/title/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const title = () => {
   return (
-    <div className=' text-white w-2/6 mt-44 mx-44 '>
+    <div className=' text-white w-2/6 mt-44'>
         <h1 className='text-center  text-5xl'>Huree ICT Технологийн клуб</h1>
         <p className='font-light tracking-widest text-justify mt-4'>Хүрээ дээд сургуулийн CS&SE тэнхэмийн харья клуб. Клуб нь Хүрээ дээд сургуулийн оюутнууд өөрсдийгөө технологийн боловсролоор хөгжүүлэхэд зориулагдсан болно. </p>
       </div>
