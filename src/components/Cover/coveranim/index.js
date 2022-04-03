023eb75ab5426baf88a2ae93ff2c2c43eb8e0e91
Munguu0623 <74600917+Munguu0623@users.noreply.github.com>
2022-04-03 16:37:57 +0800

    bg anim hiiv text oruulsan

---
+++ src/components/Cover/coveranim/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import style from "./style.module.css"
+
+const CoverAnim = () => {
+  return (
+    <div class="wrapper">
+      <div id={style.stars}></div>
+      <div id={style.stars2}></div>
+      <div id={style.stars3}></div>
+    </div>
+  );
+};
+
+export default CoverAnim;
