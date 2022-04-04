fe2c461c500e1944d1e439d69b40a803360cb32f
Munguu <mungu0623@gmail.com>
2022-04-04 06:51:05 -0700

    backround animation hudulgudg bolgow

--- src/components/Cover/coveranim/index.js
+++ src/components/Cover/coveranim/index.js
@@ -1,10 +1,9 @@
 import React from "react";
 import style from "./style.module.css";
 
-// cover effect animation od
 const CoverAnim = () => {
   return (
-    <div className="wrapper">
+    <div class="wrapper">
       <div id={style.stars}></div>
       <div id={style.stars2}></div>
       <div id={style.stars3}></div>
