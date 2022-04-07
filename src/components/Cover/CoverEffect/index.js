742fbee8279fa876bfad8cc8c0e5f52244ba8eb0
З. Эрдэнэ-Уул <zerdeneuul2001@gmail.com>
2022-04-07 14:43:41 +0800

    founders carousel zaswar1

--- src/components/Cover/CoverEffect/index.js
+++ src/components/Cover/CoverEffect/index.js
@@ -9,16 +9,16 @@ import CoverAnim from "../coveranim";
 import ScrollReveal from "scrollreveal";
 
 const CoverEffect = (props) => {
-  // useEffect(() => {
-  //   ScrollReveal().reveal("#coverimg", {
-  //     delay: 1000,
-  //     origin: "top",
-  //     duration: 1000,
-  //     distance: "500px",
-  //     scale: 1,
-  //     easing: "ease",
-  //   });
-  // }, []);
+  useEffect(() => {
+    ScrollReveal().reveal("#coverimg", {
+      delay: 1000,
+      origin: "top",
+      duration: 1000,
+      distance: "500px",
+      scale: 1,
+      easing: "ease",
+    });
+  }, []);
   return (
     <div className="bg-[#909090]/10 w-full h-full   relative m-auto" id="home">
       <CoverAnim />
