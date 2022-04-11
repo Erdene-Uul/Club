e4c7c9a10e43eb459258ed1c35a32bdf7f52c4a3
Munguu0623 <74600917+Munguu0623@users.noreply.github.com>
2022-04-11 13:46:17 +0800

    news tovch yanzalsan linkuud holobson

--- src/components/Team/Founders.js
+++ src/components/Team/Founders.js
@@ -30,10 +30,10 @@ const Founder = () => {
               Хүрээ дээд сургуулийн компьютерийн ухааны 2-р дамжааны оюутан.
             </p>
           </div>
-          <div className="flex flex-col items-center px-5 bg-[#56CB7A] w-[300px] h-[370px] rounded-[40px]">
+          <div className="flex flex-col items-center px-5 bg-[#56CB7A] w-[300px] h-[370px] rounded-[40px] transform transition duration-500 hover:scale-110">
             <img className="mt-6 mb-4 w-28 h-28" src={Uul} alt="uul" />
             <p className="font-light  text-white text-lg text-center">
-              З. Эрдэнэ-Уул 
+              З. Эрдэнэ-Уул
             </p>
             <p className="font-bold text-base mb-8 text-center">
               TechChain клубын <span className="block">Co-Founder</span>
@@ -42,7 +42,7 @@ const Founder = () => {
               Хүрээ дээд сургуулийн компьютерийн ухааны 2-р дамжааны оюутан.
             </p>
           </div>
-          <div className="flex flex-col items-center px-5 bg-[#D7A43F] w-[300px] h-[370px] rounded-[40px]">
+          <div className="flex flex-col items-center px-5 bg-[#D7A43F] w-[300px] h-[370px] rounded-[40px] transform transition duration-500 hover:scale-110">
             <img className="mt-6 mb-4 w-28 h-28" src={Uuganaa} alt="uram " />
             <p className="font-light  text-white text-lg">Д. Ууганбаяр</p>
             <p className="font-bold text-base mb-8 text-center">
--- src/pages/NewsDetail/index.js
+++ src/pages/NewsDetail/index.js
@@ -43,8 +43,25 @@ const NewsDetail = () => {
     <div className="bg-[#1C1C28] w-full h-full">
       <div className="flex justify-end ">
         <Link to="/">
-          <button className="text-white mt-3 mr-6 bg-emerald-400 px-5 rounded-xl">
-            Back
+          <button class="relative group overflow-hidden px-6 h-12 rounded-full flex space-x-2 items-center  bg-gradient-to-tr from-buttonColor1 to-buttonColor2">
+            <span class="relative text-sm text-white">Буцах</span>
+            <div class="flex items-center -space-x-3 translate-x-3">
+              <div class="w-2.5 h-[1.6px] rounded bg-white origin-left scale-x-0 transition duration-300 group-hover:scale-x-100"></div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-5 w-5 stroke-white -translate-x-2 transition duration-300 group-hover:translate-x-0"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="2"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M9 5l7 7-7 7"
+                />
+              </svg>
+            </div>
           </button>
         </Link>
       </div>
