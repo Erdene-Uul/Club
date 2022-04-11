a71accc764b3f6e2ac97ad170e3e1796acb07598
Munguu0623 <74600917+Munguu0623@users.noreply.github.com>
2022-04-11 13:56:06 +0800

    bug yanlaw

--- src/components/Team/Advisors.js
+++ src/components/Team/Advisors.js
@@ -1,14 +1,11 @@
 import React from "react";
 import Carousel from "react-elastic-carousel";
-import AdvisorsCard from "./advisorsCard";
+import Azaa from "../../images/AzbayarMunkhbayar.jpg";
+import Youngmi from "../../images/YoungMiKim.jpg";
+import Shim from "../../images/KwangCheolShin.jpg";
 
 const Advisor = () => {
-  const breakpoints = [
-    { width: 1, itemsToShow: 1, itemPadding: [10, 10] },
-    { width: 600, itemsToShow: 2, itemsToScroll: 2, itemPadding: [10, 20] },
-    { width: 850, itemsToShow: 3, itemsToScroll: 2, itemPadding: [10, 20] },
-    { width: 1000, itemsToShow: 3.5, itemsToScroll: 2, itemPadding: [10, 30] },
-  ];
+  const breakpoints = [];
   return (
     <div className="my-16 flex justify-center space-x-12 max-w-6xl mx-auto">
       <Carousel
@@ -16,7 +13,57 @@ const Advisor = () => {
         breakPoints={breakpoints}
         itemPadding={[10, 30]}
       >
-       <AdvisorsCard/>
+        <div className="flex gap-8">
+          {/* Youngmi techear */}
+          <div class=" flex items-center justify-center">
+            <a
+              class="hover:bg-gray-800 delay-50 duration-100 bg-gray-900 p-5 rounded-lg w-60 group"
+              href=""
+            >
+              <img src={Youngmi} class="w-full rounded shadow" />
+
+              <h3 class="text-gray-200 font-bold mt-5"> PHD, КИМ ЮУНГМИ</h3>
+
+              <p class="text-gray-400 font-light mt-2 text-xs">
+                {" "}
+                Компьютерийн Шинжилэх Ухааны тэнхимийн эрхлэгч багш
+              </p>
+            </a>
+          </div>
+          {/* Azbayar */}
+          <div class=" flex items-center justify-center">
+            <a
+              class="hover:bg-gray-800 delay-50 duration-100 bg-gray-900 p-5 rounded-lg w-60 group"
+              href=""
+            >
+              <img src={Azaa} class="w-full rounded shadow" />
+
+              <h3 class="text-gray-200 font-bold mt-5"> МАСТЕР М. АЗБАЯР</h3>
+
+              <p class="text-gray-400 font-light mt-2 text-xs">
+                {" "}
+                Компьютерийн Шинжилэх Ухааны тэнхимийн багш
+              </p>
+            </a>
+          </div>
+
+          {/* Shim */}
+          <div class=" flex items-center justify-center">
+            <a
+              class="hover:bg-gray-800 delay-50 duration-100 bg-gray-900 p-5 rounded-lg w-60 group"
+              href=""
+            >
+              <img src={Shim} class="w-full rounded shadow" />
+
+              <h3 class="text-gray-200 font-bold mt-5"> PHD, ШИН КВАНГЧЁОЛ</h3>
+
+              <p class="text-gray-400 font-light mt-2 text-xs">
+                {" "}
+                Компьютерийн Шинжилэх Ухааны тэнхимийн багш
+              </p>
+            </a>
+          </div>
+        </div>
       </Carousel>
     </div>
   );
