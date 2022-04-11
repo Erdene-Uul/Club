e281784678a10de2ba929b36947e74426f60f8b6
Munguu0623 <74600917+Munguu0623@users.noreply.github.com>
2022-04-11 13:32:07 +0800

    member card yanzlaw

--- src/components/Team/Member.js
+++ src/components/Team/Member.js
@@ -12,69 +12,88 @@ const Member = () => {
     { width: 1000, itemsToShow: 3.5, itemsToScroll: 2, itemPadding: [10, 30] },
   ];
   return (
-    <div className="my-16 flex justify-center space-x-12 max-w-6xl mx-auto">
-      <Carousel
-        disableArrowsOnEnd={true}
-        breakPoints={breakpoints}
-        itemPadding={[10, 30]}
-      >
+    <div className=" my-16 mx-16 flex justify-end">
+      <div className=" mx-auto grid lg:grid-cols-4 sm:grid-cols-2 gap-4  ">
+        <div class="flex items-center justify-center flex-col bg-gray-700 p-4 rounded-lg w-48 space-y-5">
+          <img
+            class="rounded-full border-gray-100 shadow-sm w-24 h-24"
+            src="https://randomuser.me/api/portraits/women/43.jpg"
+            alt="user image"
+          />
+          <h1 class="text-gray-50 font-semibold">Гансүх Болормаа</h1>
+          <h3 class="text-xl">CS1A</h3>
+        </div>
 
-     <div class="flex items-center justify-center flex-col bg-gray-700 p-4 rounded-lg w-48 space-y-5">
-         <img class="rounded-full border-gray-100 shadow-sm w-24 h-24" src="https://randomuser.me/api/portraits/women/43.jpg" alt="user image" />
-        <h1 class="text-gray-50 font-semibold">Гансүх Болормаа</h1>
-        <h3 class="text-xl">CS1A</h3>
-     </div>
+        <div class="flex items-center justify-center flex-col bg-gray-700 p-4 rounded-lg w-48 space-y-5">
+          <img
+            class="rounded-full border-gray-100 shadow-sm w-24 h-24"
+            src="https://randomuser.me/api/portraits/women/43.jpg"
+            alt="user image"
+          />
+          <h1 class="text-gray-50 font-semibold">Гансүх Болормаа</h1>
+          <h3 class="text-xl">CS1A</h3>
+        </div>
 
-     <div class="flex items-center justify-center flex-col bg-gray-700 p-4 rounded-lg w-48 space-y-5">
-         <img class="rounded-full border-gray-100 shadow-sm w-24 h-24" src="https://randomuser.me/api/portraits/women/43.jpg" alt="user image" />
-        <h1 class="text-gray-50 font-semibold">Гансүх Болормаа</h1>
-        <h3 class="text-xl">CS1A</h3>
-     </div>
-     
-     <div class="flex items-center justify-center flex-col bg-gray-700 p-4 rounded-lg w-48 space-y-5">
-         <img class="rounded-full border-gray-100 shadow-sm w-24 h-24" src="https://randomuser.me/api/portraits/women/43.jpg" alt="user image" />
-        <h1 class="text-gray-50 font-semibold">Гансүх Болормаа</h1>
-        <h3 class="text-xl">CS1A</h3>
-     </div>
-     
+        <div class="flex items-center justify-center flex-col bg-gray-700 p-4 rounded-lg w-48 space-y-5">
+          <img
+            class="rounded-full border-gray-100 shadow-sm w-24 h-24"
+            src="https://randomuser.me/api/portraits/women/43.jpg"
+            alt="user image"
+          />
+          <h1 class="text-gray-50 font-semibold">Гансүх Болормаа</h1>
+          <h3 class="text-xl">CS1A</h3>
+        </div>
 
-     <div class="flex items-center justify-center flex-col bg-gray-700 p-4 rounded-lg w-48 space-y-5">
-         <img class="rounded-full border-gray-100 shadow-sm w-24 h-24" src="https://randomuser.me/api/portraits/women/43.jpg" alt="user image" />
-        <h1 class="text-gray-50 font-semibold">Гансүх Болормаа</h1>
-        <h3 class="text-xl">CS1A</h3>
-     </div>
-     
+        <div class="flex items-center justify-center flex-col bg-gray-700 p-4 rounded-lg w-48 space-y-5">
+          <img
+            class="rounded-full border-gray-100 shadow-sm w-24 h-24"
+            src="https://randomuser.me/api/portraits/women/43.jpg"
+            alt="user image"
+          />
+          <h1 class="text-gray-50 font-semibold">Гансүх Болормаа</h1>
+          <h3 class="text-xl">CS1A</h3>
+        </div>
 
-     <div class="flex items-center justify-center flex-col bg-gray-700 p-4 rounded-lg w-48 space-y-5">
-         <img class="rounded-full border-gray-100 shadow-sm w-24 h-24" src="https://randomuser.me/api/portraits/women/43.jpg" alt="user image" />
-        <h1 class="text-gray-50 font-semibold">Гансүх Болормаа</h1>
-        <h3 class="text-xl">CS1A</h3>
-     </div>
-     
+        <div class="flex items-center justify-center flex-col bg-gray-700 p-4 rounded-lg w-48 space-y-5">
+          <img
+            class="rounded-full border-gray-100 shadow-sm w-24 h-24"
+            src="https://randomuser.me/api/portraits/women/43.jpg"
+            alt="user image"
+          />
+          <h1 class="text-gray-50 font-semibold">Гансүх Болормаа</h1>
+          <h3 class="text-xl">CS1A</h3>
+        </div>
 
-     <div class="flex items-center justify-center flex-col bg-gray-700 p-4 rounded-lg w-48 space-y-5">
-         <img class="rounded-full border-gray-100 shadow-sm w-24 h-24" src="https://randomuser.me/api/portraits/women/43.jpg" alt="user image" />
-        <h1 class="text-gray-50 font-semibold">Гансүх Болормаа</h1>
-        <h3 class="text-xl">CS1A</h3>
-     </div>
-     
+        <div class="flex items-center justify-center flex-col bg-gray-700 p-4 rounded-lg w-48 space-y-5">
+          <img
+            class="rounded-full border-gray-100 shadow-sm w-24 h-24"
+            src="https://randomuser.me/api/portraits/women/43.jpg"
+            alt="user image"
+          />
+          <h1 class="text-gray-50 font-semibold">Гансүх Болормаа</h1>
+          <h3 class="text-xl">CS1A</h3>
+        </div>
 
-     <div class="flex items-center justify-center flex-col bg-gray-700 p-4 rounded-lg w-48 space-y-5">
-         <img class="rounded-full border-gray-100 shadow-sm w-24 h-24" src="https://randomuser.me/api/portraits/women/43.jpg" alt="user image" />
-        <h1 class="text-gray-50 font-semibold">Гансүх Болормаа</h1>
-        <h3 class="text-xl">CS1A</h3>
-     </div>
-     
+        <div class="flex items-center justify-center flex-col bg-gray-700 p-4 rounded-lg w-48 space-y-5">
+          <img
+            class="rounded-full border-gray-100 shadow-sm w-24 h-24"
+            src="https://randomuser.me/api/portraits/women/43.jpg"
+            alt="user image"
+          />
+          <h1 class="text-gray-50 font-semibold">Гансүх Болормаа</h1>
+          <h3 class="text-xl">CS1A</h3>
+        </div>
 
-     <div class="flex items-center justify-center flex-col bg-gray-700 p-4 rounded-lg w-48 space-y-5">
-         <img class="rounded-full border-gray-100 shadow-sm w-24 h-24" src="https://randomuser.me/api/portraits/women/43.jpg" alt="user image" />
-        <h1 class="text-gray-50 font-semibold">Гансүх Болормаа</h1>
-        <h3 class="text-xl">CS1A</h3>
-     </div>
-     
-     
-    
-      </Carousel>
+        <div class="flex items-center justify-center flex-col bg-gray-700 p-4 rounded-lg w-48 space-y-5">
+          <img
+            class="rounded-full border-gray-100 shadow-sm w-24 h-24"
+            src="https://randomuser.me/api/portraits/women/43.jpg"
+            alt="user image"
+          />
+          <h1 class="text-gray-50 font-semibold">Гансүх Болормаа</h1>
+          <h3 class="text-xl">CS1A</h3>
+        </div>
+      </div>
     </div>
   );
 };
