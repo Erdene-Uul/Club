ed0377a0a797270242396e4032d7a673869a6f3d
З. Эрдэнэ-Уул <zerdeneuul2001@gmail.com>
2022-04-12 23:41:47 +0800

    nothing

--- src/components/Team/Advisors.js
+++ src/components/Team/Advisors.js
@@ -3,73 +3,66 @@ import Carousel from "react-elastic-carousel";
 import Azaa from "../../images/AzbayarMunkhbayar.jpg";
 import Youngmi from "../../images/YoungMiKim.jpg";
 import Shim from "../../images/KwangCheolShin.jpg";
-import { Link } from "react-router-dom";
 
 const Advisor = () => {
-  const breakpoints = [];
+  const breakpoints = [
+    { width: 1, itemsToShow: 1, itemPadding: [10, 10] },
+    { width: 600, itemsToShow: 2, itemsToScroll: 2, itemPadding: [10, 20] },
+    { width: 850, itemsToShow: 3, itemsToScroll: 2, itemPadding: [10, 20] },
+    { width: 1000, itemsToShow: 3.5, itemsToScroll: 2, itemPadding: [10, 30] },
+  ];
   return (
     <div className="my-16 flex justify-center space-x-12 max-w-6xl mx-auto">
-      <Carousel
-        disableArrowsOnEnd={true}
-        breakPoints={breakpoints}
-        itemPadding={[10, 30]}
-      >
-        <div className="flex gap-8">
+      <div className="flex gap-8">
+        <Carousel
+          disableArrowsOnEnd={true}
+          breakPoints={breakpoints}
+          itemPadding={[10, 30]}
+        >
           {/* Youngmi techear */}
-          <div class=" flex items-center justify-center">
-            <Link
-              to={"/"}
-              class="hover:bg-gray-800 delay-50 duration-100 bg-gray-900 p-5 rounded-lg w-60 group"
-            >
-              <img
-                src={Youngmi}
-                class="w-full rounded shadow"
-                alt="youngmi pro"
-              />
+          <div className="hover:bg-gray-800 delay-50 duration-100 bg-gray-900 p-5 rounded-lg w-60 group flex items-center justify-center">
+            <img
+              src={Youngmi}
+              className="w-full rounded shadow"
+              alt="youngmi pro"
+            />
 
-              <h3 class="text-gray-200 font-bold mt-5"> PHD, КИМ ЮУНГМИ</h3>
+            <h3 className="text-gray-200 font-bold mt-5"> PHD, КИМ ЮУНГМИ</h3>
 
-              <p class="text-gray-400 font-light mt-2 text-xs">
-                {" "}
-                Компьютерийн Шинжилэх Ухааны тэнхимийн эрхлэгч багш
-              </p>
-            </Link>
+            <p className="text-gray-400 font-light mt-2 text-xs">
+              {" "}
+              Компьютерийн Шинжилэх Ухааны тэнхимийн эрхлэгч багш
+            </p>
           </div>
+
           {/* Azbayar */}
-          <div class=" flex items-center justify-center">
-            <Link
-              to={"/"}
-              class="hover:bg-gray-800 delay-50 duration-100 bg-gray-900 p-5 rounded-lg w-60 group"
-            >
-              <img src={Azaa} class="w-full rounded shadow" alt="azaa pro" />
+          <div className="hover:bg-gray-800 delay-50 duration-100 bg-gray-900 p-5 rounded-lg w-60 group flex items-center justify-center">
+            <img src={Azaa} className="w-full rounded shadow" alt="azaa pro" />
 
-              <h3 class="text-gray-200 font-bold mt-5"> МАСТЕР М. АЗБАЯР</h3>
+            <h3 className="text-gray-200 font-bold mt-5"> МАСТЕР М. АЗБАЯР</h3>
 
-              <p class="text-gray-400 font-light mt-2 text-xs">
-                {" "}
-                Компьютерийн Шинжилэх Ухааны тэнхимийн багш
-              </p>
-            </Link>
+            <p className="text-gray-400 font-light mt-2 text-xs">
+              {" "}
+              Компьютерийн Шинжилэх Ухааны тэнхимийн багш
+            </p>
           </div>
 
           {/* Shim */}
-          <div class=" flex items-center justify-center">
-            <Link
-              to={"/"}
-              class="hover:bg-gray-800 delay-50 duration-100 bg-gray-900 p-5 rounded-lg w-60 group"
-            >
-              <img src={Shim} class="w-full rounded shadow" alt="shim pro" />
+          <div className="hover:bg-gray-800 delay-50 duration-100 bg-gray-900 p-5 rounded-lg w-60 group flex items-center justify-center">
+            <img src={Shim} className="w-full rounded shadow" alt="shim pro" />
 
-              <h3 class="text-gray-200 font-bold mt-5"> PHD, ШИН КВАНГЧЁОЛ</h3>
+            <h3 className="text-gray-200 font-bold mt-5">
+              {" "}
+              PHD, ШИН КВАНГЧЁОЛ
+            </h3>
 
-              <p class="text-gray-400 font-light mt-2 text-xs">
-                {" "}
-                Компьютерийн Шинжилэх Ухааны тэнхимийн багш
-              </p>
-            </Link>
+            <p className="text-gray-400 font-light mt-2 text-xs">
+              {" "}
+              Компьютерийн Шинжилэх Ухааны тэнхимийн багш
+            </p>
           </div>
-        </div>
-      </Carousel>
+        </Carousel>
+      </div>
     </div>
   );
 };
--- src/components/Team/Member.js
+++ src/components/Team/Member.js
@@ -4,84 +4,84 @@ const Member = () => {
   return (
     <div className=" my-16 mx-16 flex justify-end">
       <div className=" mx-auto grid lg:grid-cols-4 sm:grid-cols-2 gap-4  ">
-        <div class="flex items-center justify-center flex-col bg-gray-700 p-4 rounded-lg w-48 space-y-5">
+        <div className="flex items-center justify-center flex-col bg-gray-700 p-4 rounded-lg w-48 space-y-5">
           <img
-            class="rounded-full border-gray-100 shadow-sm w-24 h-24"
+            className="rounded-full border-gray-100 shadow-sm w-24 h-24"
             src="https://randomuser.me/api/portraits/women/43.jpg"
             alt="user image7"
           />
-          <h1 class="text-gray-50 font-semibold">Гансүх Болормаа</h1>
-          <h3 class="text-xl">CS1A</h3>
+          <h1 className="text-gray-50 font-semibold">Гансүх Болормаа</h1>
+          <h3 className="text-xl">CS1A</h3>
         </div>
 
-        <div class="flex items-center justify-center flex-col bg-gray-700 p-4 rounded-lg w-48 space-y-5">
+        <div className="flex items-center justify-center flex-col bg-gray-700 p-4 rounded-lg w-48 space-y-5">
           <img
-            class="rounded-full border-gray-100 shadow-sm w-24 h-24"
+            className="rounded-full border-gray-100 shadow-sm w-24 h-24"
             src="https://randomuser.me/api/portraits/women/43.jpg"
             alt="user image8"
           />
-          <h1 class="text-gray-50 font-semibold">Гансүх Болормаа</h1>
-          <h3 class="text-xl">CS1A</h3>
+          <h1 className="text-gray-50 font-semibold">Гансүх Болормаа</h1>
+          <h3 className="text-xl">CS1A</h3>
         </div>
 
-        <div class="flex items-center justify-center flex-col bg-gray-700 p-4 rounded-lg w-48 space-y-5">
+        <div className="flex items-center justify-center flex-col bg-gray-700 p-4 rounded-lg w-48 space-y-5">
           <img
-            class="rounded-full border-gray-100 shadow-sm w-24 h-24"
+            className="rounded-full border-gray-100 shadow-sm w-24 h-24"
             src="https://randomuser.me/api/portraits/women/43.jpg"
             alt="user image1"
           />
-          <h1 class="text-gray-50 font-semibold">Гансүх Болормаа</h1>
-          <h3 class="text-xl">CS1A</h3>
+          <h1 className="text-gray-50 font-semibold">Гансүх Болормаа</h1>
+          <h3 className="text-xl">CS1A</h3>
         </div>
 
-        <div class="flex items-center justify-center flex-col bg-gray-700 p-4 rounded-lg w-48 space-y-5">
+        <div className="flex items-center justify-center flex-col bg-gray-700 p-4 rounded-lg w-48 space-y-5">
           <img
-            class="rounded-full border-gray-100 shadow-sm w-24 h-24"
+            className="rounded-full border-gray-100 shadow-sm w-24 h-24"
             src="https://randomuser.me/api/portraits/women/43.jpg"
             alt="user image2"
           />
-          <h1 class="text-gray-50 font-semibold">Гансүх Болормаа</h1>
-          <h3 class="text-xl">CS1A</h3>
+          <h1 className="text-gray-50 font-semibold">Гансүх Болормаа</h1>
+          <h3 className="text-xl">CS1A</h3>
         </div>
 
-        <div class="flex items-center justify-center flex-col bg-gray-700 p-4 rounded-lg w-48 space-y-5">
+        <div className="flex items-center justify-center flex-col bg-gray-700 p-4 rounded-lg w-48 space-y-5">
           <img
-            class="rounded-full border-gray-100 shadow-sm w-24 h-24"
+            className="rounded-full border-gray-100 shadow-sm w-24 h-24"
             src="https://randomuser.me/api/portraits/women/43.jpg"
             alt="user image3"
           />
-          <h1 class="text-gray-50 font-semibold">Гансүх Болормаа</h1>
-          <h3 class="text-xl">CS1A</h3>
+          <h1 className="text-gray-50 font-semibold">Гансүх Болормаа</h1>
+          <h3 className="text-xl">CS1A</h3>
         </div>
 
-        <div class="flex items-center justify-center flex-col bg-gray-700 p-4 rounded-lg w-48 space-y-5">
+        <div className="flex items-center justify-center flex-col bg-gray-700 p-4 rounded-lg w-48 space-y-5">
           <img
-            class="rounded-full border-gray-100 shadow-sm w-24 h-24"
+            className="rounded-full border-gray-100 shadow-sm w-24 h-24"
             src="https://randomuser.me/api/portraits/women/43.jpg"
             alt="user image4"
           />
-          <h1 class="text-gray-50 font-semibold">Гансүх Болормаа</h1>
-          <h3 class="text-xl">CS1A</h3>
+          <h1 className="text-gray-50 font-semibold">Гансүх Болормаа</h1>
+          <h3 className="text-xl">CS1A</h3>
         </div>
 
-        <div class="flex items-center justify-center flex-col bg-gray-700 p-4 rounded-lg w-48 space-y-5">
+        <div className="flex items-center justify-center flex-col bg-gray-700 p-4 rounded-lg w-48 space-y-5">
           <img
-            class="rounded-full border-gray-100 shadow-sm w-24 h-24"
+            className="rounded-full border-gray-100 shadow-sm w-24 h-24"
             src="https://randomuser.me/api/portraits/women/43.jpg"
             alt="user image5"
           />
-          <h1 class="text-gray-50 font-semibold">Гансүх Болормаа</h1>
-          <h3 class="text-xl">CS1A</h3>
+          <h1 className="text-gray-50 font-semibold">Гансүх Болормаа</h1>
+          <h3 className="text-xl">CS1A</h3>
         </div>
 
-        <div class="flex items-center justify-center flex-col bg-gray-700 p-4 rounded-lg w-48 space-y-5">
+        <div className="flex items-center justify-center flex-col bg-gray-700 p-4 rounded-lg w-48 space-y-5">
           <img
-            class="rounded-full border-gray-100 shadow-sm w-24 h-24"
+            className="rounded-full border-gray-100 shadow-sm w-24 h-24"
             src="https://randomuser.me/api/portraits/women/43.jpg"
             alt="user image6"
           />
-          <h1 class="text-gray-50 font-semibold">Гансүх Болормаа</h1>
-          <h3 class="text-xl">CS1A</h3>
+          <h1 className="text-gray-50 font-semibold">Гансүх Болормаа</h1>
+          <h3 className="text-xl">CS1A</h3>
         </div>
       </div>
     </div>
