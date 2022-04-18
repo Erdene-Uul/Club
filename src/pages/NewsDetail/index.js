e55e7016c623c98eb8c9c17c39982ae2c305e78c
З. Эрдэнэ-Уул <zerdeneuul2001@gmail.com>
2022-04-18 15:54:13 +0800

    netlify zaswar

--- src/pages/NewsDetail/index.js
+++ src/pages/NewsDetail/index.js
@@ -7,11 +7,23 @@ import moment from "moment";
 import Footer from "../../components/Footer";
 import Gif from "./gif";
 import "./style.module.css";
+import {ShareSocial} from 'react-share-social' 
+
+const style = {
+  background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+  borderRadius: 3,
+  border: 0,
+  color: 'white',
+  padding: '0 30px',
+  boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+};
 
 const NewsDetail = () => {
   const [show, setShow] = useState(true);
   const [news, setNews] = useState();
   const { slug } = useParams();
+  const lol = useParams();
+  console.log(lol.slug);
   useEffect(() => {
     sanityClient
       .fetch(
@@ -80,6 +92,11 @@ const NewsDetail = () => {
           </div>
         </div>
       </div>
+      <ShareSocial 
+     style={style}
+     url ={`https://hureechain.netlify.app/${lol.slug}`}
+     socialTypes={['facebook','twitter','reddit','linkedin']}
+   />
       <Footer />
     </div>
   );
