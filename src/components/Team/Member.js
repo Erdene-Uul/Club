fedfd56067c21ebd416cf3efd6d9ffbe051b669e
З. Эрдэнэ-Уул <zerdeneuul2001@gmail.com>
2022-09-15 09:23:21 +0800

    uul zaswar 2

--- src/components/Team/Member.js
+++ src/components/Team/Member.js
@@ -5,7 +5,7 @@ const Member = () => {
   return (
     <div className="mt-16">
       <div className=" mx-auto max-w-4xl flex flex-wrap justify-center">
-        <ClubMembers name="Гансүх Болормаа" dep="CS1A" />
+        <ClubMembers name="Гансүх Болормаа" dep="CS2A" />
         <ClubMembers name="Гансүх Болормаа" dep="CS1A" />
         <ClubMembers name="Гансүх Болормаа" dep="CS1A" />
         <ClubMembers name="Гансүх Болормаа" dep="CS1A" />
